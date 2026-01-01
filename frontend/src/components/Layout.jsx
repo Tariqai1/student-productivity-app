0e0913199221530b3da8fc07c55b9c521bb6d853
Muhammad Tarique <tariqfsd9@gmail.com>
2026-01-01 13:56:42 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
+
   return (
     <div className="min-h-screen bg-slate-50 flex">
       {/* Sidebar Fixed */}
-      <Sidebar />
+      <Sidebar/>
       
       {/* Main Content (Pushed right by w-72 which is 18rem) */}
-      <main className="flex-1 ml-72 p-8 transition-all duration-300">
+      <main className={`flex-1 transition-all duration-30 p-8`}>
         <div className="max-w-7xl mx-auto animate-fadeIn">
           {children}
         </div>
